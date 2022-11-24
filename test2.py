25fa3660640c23efb3976775e98b4164adef9f08
vaibhav-vt <77973068+vaibhav-vt@users.noreply.github.com>
2022-11-25 00:14:30 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -6,13 +6,13 @@ class TestDiv(unittest.TestCase):
 	def test_4(self):
 		a_4 = 87
 		b_4 = 8
-		result = divide(a_1, b_1)
+		result = divide(a_4, b_4)
 		self.assertEqual(result, 7)
 		
 	def test_2(self):
 		a_5 = 340
 		b_5 = 23
-		result = divide(a_2, b_2)
+		result = divide(a_5, b_5)
 		self.assertEqual(result, 11)
     
 
