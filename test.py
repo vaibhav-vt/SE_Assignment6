693d369bbbce35e810bd71472532f7022a579c2d
vaibhav-vt <77973068+vaibhav-vt@users.noreply.github.com>
2022-11-24 23:57:59 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,37 +1,26 @@
 import unittest
 
-from program import multiply
+from program import divide
 
 class TestSum(unittest.TestCase):
 	def test_1(self):
-		a_1 = 10
+		a_1 = 30
 		b_1 = 10
-		result = multiply(a_1, b_1)
-		self.assertEqual(result, 100)
+		result = divide(a_1, b_1)
+		self.assertEqual(result, 3)
 		
 	def test_2(self):
 		a_2 = 10
 		b_2 = 5
-		result = multiply(a_2, b_2)
-		self.assertEqual(result, 50)
+		result = divide(a_2, b_2)
+		self.assertEqual(result, 2)
 
 	def test_3(self):
-		a_3 = 5
-		b_3 = 5
-		result = multiply(a_3, b_3)
-		self.assertEqual(result, 25)
+		a_3 = 66
+		b_3 = 6
+		result = divide(a_3, b_3)
+		self.assertEqual(result, 11)
 
-	def test_4(self):
-		a_4 = 2
-		b_4 = 5
-		result = multiply(a_4, b_4)
-		self.assertEqual(result, 20) 
-
-	def test_5(self):
-		a_5 = 5
-		b_5 = 4
-		result = multiply(a_5, b_5)
-		self.assertEqual(result, 25)
 
 if __name__ == '__main__':
 	unittest.main()
